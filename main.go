ecc49056956aa47fc74a38c8075106e1c8602af8
Andrew <93141508+svpra@users.noreply.github.com>
2022-10-12 23:31:17 +0300

    Update main.go

--- main.go
+++ main.go
@@ -2,9 +2,30 @@ package main
 
 import (
     "fmt"
+    "log"
     "http/net"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/hello" {
+        http.Error(w, "404 not found.", http.StatusNotFound)
+        return
+    }
+
+    if r.Method != "GET" {
+        http.Error(w, "Method is not supported.", http.StatusNotFound)
+        return
+    }
+
+
+    fmt.Fprintf(w, "Hello!")
+}
+
 func main() {
-    http.Open
+    http.HandleFunc("/hello", helloHandler)
+
+    fmt.Printf("Hello! This is my first Golang Server!\n")
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        log.Fatal(err)
+    }
 }
