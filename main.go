d8a38ed2ae5cab5b9b20865e0cab1b057a3dbe25
Andrew <93141508+svpra@users.noreply.github.com>
2022-10-12 23:42:14 +0300

    Added formHandler

--- main.go
+++ main.go
@@ -6,6 +6,18 @@ import (
     "http/net"
 )
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+    if err := r.ParseForm(); err != nil {
+        fmt.Fprintf(w, "ParseForm() err: %v", err)
+        return
+    }
+    fmt.Fprintf(w, "POST request successful")
+    name := r.FormValue("name")
+    address := r.FormValue("address")
+    fmt.Fprintf(w, "Name = %s\n", name)
+    fmt.Fprintf(w, "Address = %s\n", address)
+}
+
 func helloHandler(w http.ResponseWriter, r *http.Request) {
     if r.URL.Path != "/hello" {
         http.Error(w, "404 not found.", http.StatusNotFound)
@@ -17,11 +29,13 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-
     fmt.Fprintf(w, "Hello!")
 }
 
 func main() {
+    fileServer := http.FileServer(http.Dir("./static"))
+    http.Handle("/", fileServer)
+    http.HandleFunc("/form", formHandler)
     http.HandleFunc("/hello", helloHandler)
 
     fmt.Printf("Hello! This is my first Golang Server!\n")
